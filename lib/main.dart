45782172241719a41305c3d746543ec00d97611e
Luciano Jung <LucianoJ@posteo.de>
2021-06-06 22:46:12 +0200

    fix initial route

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: themeData(),
       onGenerateRoute: RouteGenerator.generateRoute,
-      initialRoute: '/convex_bottom_bar',
+      initialRoute: '/',
     );
   }
 
